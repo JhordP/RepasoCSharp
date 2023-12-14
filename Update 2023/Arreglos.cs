0b10f2b4099de91bdd4beca665938e427000387b
JhordP <j.horda.ny@hotmail.com>
2023-12-14 18:16:35 -0400

    Arreglos y listas 2023

---
+++ Update 2023/Arreglos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica.Update_2023
+{
+    public class Arreglos
+    {
+        int[] ints;
+
+        public Arreglos()
+        {
+            //Sintaxis de arreglos
+            ints = new int[3] { 1, 2, 3}; //Tambien se le puede colocar valores por defecto entre llaves
+
+            //Acceder a las variables
+            int numero = ints[0]; //Primer elemento, desde cero en adelante
+        }
+    }
+}
---
+++ Update 2023/Listas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica.Update_2023
+{
+    public class Listas
+    {
+        public Listas()
+        {
+             
+        }
+    }
+}
